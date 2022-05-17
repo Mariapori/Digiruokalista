c755911473003576633e0ea80810f027b23a9b80
Topias Mariapori <topias@mariapori.fi>
2022-05-17 23:49:50 +0300

    Oma emailer luokka

---
+++ Digiruokalista-Remastered/Common/CustomEmailer.cs
@@ -0,0 +1,33 @@
+﻿using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using MimeKit;
+
+namespace Digiruokalista_Remastered.Common
+{
+    public class CustomEmailer : IEmailSender
+    {
+        public string host { get; set; } = null!;
+        public string password { get; set; } = null!;
+        public int port { get; set; } = default!;
+        public bool useSSL { get; set; } = default!;
+        public string email { get; set; } = null!;
+
+        public Task SendEmailAsync(string toEmail, string subject, string htmlMessage)
+        {
+            using(var smtpClient = new SmtpClient())
+            {
+                smtpClient.Connect(host, port,useSSL);
+                smtpClient.Authenticate(email, password);
+                var viesti = new MimeMessage();
+                viesti.From.Add(InternetAddress.Parse(email));
+                viesti.To.Add(InternetAddress.Parse(toEmail));
+                viesti.Subject = subject;
+                var bodybuilder = new BodyBuilder();
+                bodybuilder.HtmlBody = htmlMessage;
+                viesti.Body = bodybuilder.ToMessageBody();
+                smtpClient.SendAsync(viesti);
+            }
+            return Task.CompletedTask;
+        }
+    }
+}
--- Digiruokalista-Remastered/Program.cs
+++ Digiruokalista-Remastered/Program.cs
@@ -6,9 +6,20 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.EntityFrameworkCore;
+using Digiruokalista_Remastered.Common;
+using Microsoft.AspNetCore.Identity.UI.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddTransient<IEmailSender, Digiruokalista_Remastered.Common.CustomEmailer>();
+builder.Services.Configure<CustomEmailer>(options =>
+{
+    options.host = builder.Configuration.GetSection("smtpSettings")["smtpHost"];
+    options.port = int.Parse(builder.Configuration.GetSection("stmpSettings")["smtpPort"]);
+    options.useSSL = bool.Parse(builder.Configuration.GetSection("smtpSettings")["useSSL"]);
+    options.email = builder.Configuration.GetSection("smtpSettings")["email"];
+    options.password = builder.Configuration.GetSection("smtpSettings")["password"];
+});
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
